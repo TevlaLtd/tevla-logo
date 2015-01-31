114bcaca00377f8fb3080e1972d0b8fc2a98319d
tevla <vladimir@tevla.co.uk>
2015-01-31 19:42:35 +0000

    logo data mapped

--- js/index.js
+++ js/index.js
@@ -1,34 +1,136 @@
 (function(){ 
+
+	var _interval;
+	var _index=0;
+	var _cells;
+	var _nbCells;
+
+
+	var data = {};
+	//T
+		data["00"] = 1;
+		data["10"] = 1;
+		data["20"] = 1;
+		data["30"] = 1;
+		data["40"] = 1;
+		data["21"] = 1;
+		data["22"] = 1;
+		data["23"] = 1;
+		data["24"] = 1;
+	//E
+		data["60"] = 1;
+		data["61"] = 1;
+		data["62"] = 1;
+		data["63"] = 1;
+		data["64"] = 1;
+		data["70"] = 1;
+		data["80"] = 1;
+		data["90"] = 1;
+		// data["100"] = 1;
+		data["72"] = 1;
+		data["82"] = 1;
+		data["92"] = 1;
+		// data["102"] = 1;
+		data["74"] = 1;
+		data["84"] = 1;
+		data["94"] = 1;
+		// data["104"] = 1;
+	//V
+		data["110"] = 1;
+		data["111"] = 1;
+		data["112"] = 1;
+		data["123"] = 1;
+		data["134"] = 1;
+		data["143"] = 1;
+		data["152"] = 1;
+		data["151"] = 1;
+		data["150"] = 1;
+
+	//L
+		data["170"] = 1;		
+		data["171"] = 1;		
+		data["172"] = 1;		
+		data["173"] = 1;		
+		data["174"] = 1;		
+		data["184"] = 1;		
+		data["194"] = 1;		
+		data["204"] = 1;		
+		// data["224"] = 1;		
+
+	//A
+		data["221"] = 1;
+		data["222"] = 1;
+		data["223"] = 1;
+		data["224"] = 1;
+		data["230"] = 1;
+		data["240"] = 1;
+		data["250"] = 1;
+		data["261"] = 1;
+		data["262"] = 1;
+		data["263"] = 1;
+		data["264"] = 1;
+		data["232"] = 1;		
+		data["242"] = 1;		
+		data["252"] = 1;
+
 	initArray();
-	animateArray();
-}()); 
+	_interval = setInterval(animateArray,50);
+	// animateArray();
 
-function initArray(){
-	console.log("initArray");
 
-	var logo = document.getElementById("logo");
+	function initArray(){
 
-	var table = document.createElement("table");
+		var logo = document.getElementById("logo");
 
-	var nbRows = 5;
-	var nbCols = 5;
+		var table = document.createElement("table");
+		table.id="pixels";
 
-	for (var i=0; i<nbRows; i++){
-		var tr = document.createElement("tr");
-		for (var j=0; j<nbCols; j++){
-			var td = document.createElement("td");
-			td.className="logopixel";
+		var nbX = 30;
+		var nbY = 5;
 
-			tr.appendChild(td);
+		var i=0;
+		var j=0;
+		var ji="";
+		var td, tr;
+
+		for (i=0; i<nbY; i++){
+			tr = document.createElement("tr");
+			for (j=0; j<nbX; j++){
+				td = document.createElement("td");
+				td.id="cell";
+				ji = String(j)+String(i);
+				if (data[ji])
+					td.className="active";
+				else td.className="logopixel";
+
+				tr.appendChild(td);
+			}
+			table.appendChild(tr);
 		}
-		table.appendChild(tr);
+
+		logo.appendChild(table);
+
+		_cells = document.getElementsByClassName("active");
+		_nbCells = _cells.length;
 	}
 
-	logo.appendChild(table);
-}
+	function animateArray(){
+		
+		// _cells[_index].style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+		try {
+			_cells[Number(_index-1)].style.backgroundColor = '#000';
+		} catch (e){
+		}
 
-function animateArray(){
-	//var cells = document.getElementsByClassName("logopixel");
+		// _cells[_index].style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+		// _cells[_index].style.backgroundColor = '#fff';
 
-	
-}
+		_index++;
+
+		if (_index>_nbCells)
+			clearInterval(_interval)
+	}
+
+}()); 
+
+
